71001c4a5b43bee6acb6ed7a6f425f1aafb5de60
kenshin0413 <kenshin1234569@gmail.com>
2025-10-08 14:21:12 +0900

    初期

---
+++ Review-app/Review-app/Review_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  Review_appApp.swift
+//  Review-app
+//
+//  Created by miyamotokenshin on R 7/10/08.
+//
+
+import SwiftUI
+
+@main
+struct Review_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
